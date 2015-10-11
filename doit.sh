92459f51b0acb411c7599d4a8a7c2ee9a3c77551
Jim McBride <jm@gonkulator.io>
2015-10-11 12:40:12 -0500

    added "-o LogLevel=FATAL -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes" to each scp and ssh to get vmware to deal

--- doit.sh
+++ doit.sh
@@ -21,8 +21,8 @@ mastersshkey=$(cat sshconfigs/cnvm-00-sshconfig | grep IdentityFile | awk '{prin
 masterport=$(cat sshconfigs/cnvm-00-sshconfig | head -4 | grep Port | awk '{print $2}')
 
 ssh-keyscan -p ${masterport} -t rsa ${masterip} >> ~/.ssh/known_hosts 
-ssh -p ${masterport} -i ${mastersshkey} ${masteruser}@${masterip} "sudo cp id_rsa* /root/.ssh && sudo chown root /root/.ssh/id_rsa*"
-scp -P ${masterport} -i ${mastersshkey} ${masteruser}@${masterip}:./id_rsa* ./thekeys
+ssh -p ${masterport} -i ${mastersshkey} ${masteruser}@${masterip} -o LogLevel=FATAL -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes "sudo cp id_rsa* /root/.ssh && sudo chown root /root/.ssh/id_rsa*"
+scp -P ${masterport} -i ${mastersshkey} -o LogLevel=FATAL -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes ${masteruser}@${masterip}:./id_rsa* ./thekeys
 echo "Retrieved cnvm-00 ssh-keys"
 
 targetnodes=($(cat therunninghosts | grep -v cnvm-00))
@@ -37,9 +37,9 @@ for i in ${targetnodes[@]}; do
 	targetport=$(cat sshconfigs/$i-sshconfig | head -4 | grep Port | awk '{print $2}')
 	echo "Connecting to $i to do the key needful..."
 	ssh-keyscan -p ${targetport} -t rsa ${targetip} >> ~/.ssh/known_hosts 
-	ssh -p ${targetport} -i ${targetkey} ${targetuser}@${targetip} mkdir nodekeys
-	scp -P ${targetport} -i ${targetkey} ./thekeys/* ${targetuser}@${targetip}:./nodekeys
-	ssh -p ${targetport} -i ${targetkey} ${targetuser}@${targetip} 'sudo ~/nodekeys/keyupdate.sh'
+	ssh -p ${targetport} -i ${targetkey} ${targetuser}@${targetip} -o LogLevel=FATAL -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes mkdir nodekeys
+	scp -P ${targetport} -i ${targetkey} -o LogLevel=FATAL -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes ./thekeys/* ${targetuser}@${targetip}:./nodekeys
+	ssh -p ${targetport} -i ${targetkey} ${targetuser}@${targetip} -o LogLevel=FATAL -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes 'sudo ~/nodekeys/keyupdate.sh'
 done
 
 keyscantargets=$(cd ./sshconfigs && for i in $(ls) ; do cat $i | head -2 | grep HostName | awk '{print $2}' ;done | xargs)
@@ -49,19 +49,19 @@ for i in ${targetnodes[@]}; do
 	targetuser=$(cat sshconfigs/$i-sshconfig | head -3 | grep User | awk '{print $2}')
 	targetkey=$(cat sshconfigs/$i-sshconfig | grep IdentityFile | awk '{print $2}')
 	targetport=$(cat sshconfigs/$i-sshconfig | head -4 | grep Port | awk '{print $2}')
-	ssh -p ${targetport} -i ${targetkey} ${targetuser}@${targetip} "sudo ~/nodekeys/keyscanner.sh ${keyscantargets}"
+	ssh -p ${targetport} -i ${targetkey} ${targetuser}@${targetip} -o LogLevel=FATAL -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes "sudo ~/nodekeys/keyscanner.sh ${keyscantargets}"
 done
 
 echo "Keyscanning master to targets..."
-scp -P ${masterport} -i ${mastersshkey} thekeys/keyscanner.sh ${masteruser}@${masterip}:.
-ssh -p ${masterport} -i ${mastersshkey} ${masteruser}@${masterip} "sudo ~/keyscanner.sh ${keyscantargets}"
+scp -P ${masterport} -i ${mastersshkey} -o LogLevel=FATAL -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes thekeys/keyscanner.sh ${masteruser}@${masterip}:.
+ssh -p ${masterport} -i ${mastersshkey} ${masteruser}@${masterip} -o LogLevel=FATAL -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes "sudo ~/keyscanner.sh ${keyscantargets}"
 
 echo "Kicking off Cloud Native VM footlocker builds..."
 footlockertargets=$(cd ./sshconfigs && for i in $(ls | grep -v cnvm-00) ; do cat $i | head -2 | grep HostName | awk '{print $2}' ;done | xargs | sed s/\ /,/g)
 echo "Pulling build container...."
-ssh -p ${masterport} -i ${mastersshkey} ${masteruser}@${masterip} "docker pull gonkulatorlabs/cnvm"
+ssh -p ${masterport} -i ${mastersshkey} ${masteruser}@${masterip} -o LogLevel=FATAL -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes "docker pull gonkulatorlabs/cnvm"
 echo "Building...."
-ssh -p ${masterport} -i ${mastersshkey} ${masteruser}@${masterip} "sudo docker run -v /root/.ssh/id_rsa:/keys/priv -v /root/.ssh/id_rsa.pub:/keys/pub -e NODES=${footlockertargets} gonkulatorlabs/cnvm"
+ssh -p ${masterport} -i ${mastersshkey} ${masteruser}@${masterip} -o LogLevel=FATAL -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes "sudo docker run -v /root/.ssh/id_rsa:/keys/priv -v /root/.ssh/id_rsa.pub:/keys/pub -e NODES=${footlockertargets} gonkulatorlabs/cnvm"
 
 echo "Cleaning up..."
 rm sshconfigs/*
